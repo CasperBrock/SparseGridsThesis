8eb8a6fb474209f2fd6c78605e09b85925c11a6a
Brock <casb@itu.dk>
2015-02-13 13:23:49 +0100

    changes

--- SparseGridsJava/src/testSuite.java
+++ SparseGridsJava/src/testSuite.java
@@ -23,24 +23,21 @@ public class testSuite {
 		System.out.println("Running tests on "+cores+" cores using up to "+mem+" MB of memory.");
 
 		for (int i = 2; i<7; i++){// this loop will relate to the number of dimensions, as this scales strongest		
-			for (int j = 2; j<7;j++){// this loop will relate to number of levels.
+			for (int j = 2; j<6;j++){// this loop will relate to number of levels.
 				for (k = 1; k<cores*2;k++){//this loop iterates over number of threads.
+					System.out.println("Test with " + i + " dimensions, all with level " + j + ", running in "+k+" threads.");
 					for (int l = 1;l<10;l++){
 
+						time = 0;
 						int best = Integer.MAX_VALUE;
 						int[] levels = new int[i]; //array of levels, must be as long as the amount of dimensions.
 
 						//lines that create the objects we are to work on. Should not be timed, ofc.
 						Arrays.fill(levels, j);
 						CG = new CombiGrid(i, levels);
-						Arrays.fill(CG.grid, 1);
-
-						System.out.println("Test with " + i + " dimensions, all with level " + j + ", running in "+k+" threads.");
-
+						Arrays.fill(CG.grid, 1);					
 						millisStart = System.currentTimeMillis();
-
 						CG.hierarchizeUnoptimizedThreads(k);
-
 						//	CG.printValues(); //print only to check that everything is being calculated correctly. Takes a lot of extra time.
 						millisStop = System.currentTimeMillis();
 
